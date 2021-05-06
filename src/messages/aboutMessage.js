4aa61368dc31474252f301c1d6fa30ea6c680c9e
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-05-06 20:23:50 +0200

    Create message to 'About' component

---
+++ src/messages/aboutMessage.js
@@ -0,0 +1,13 @@
+export let message ={
+    EN:{
+        hi:"Hi, my name is",
+        about:"EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN EN",
+    },
+    PL:{
+        hi:"Cześć, nazywam się",
+        about:"PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL PL",
+    },
+    const:{
+        name:"Cezary Chojnowski",
+    }
+}
