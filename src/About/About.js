cf70e5cdb016e04966cd4702fbe142d012134269
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-05-06 20:24:20 +0200

    Create 'About' component and required style

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
-import { message } from '../messages/tempMessage'
+import { message } from '../messages/aboutMessage'
 import { LanguageContext } from '../Contexts/LanguageContext'
+import './about.css'
 
 function About() {
   const { language } = useContext(LanguageContext)
   return (
-    <div>
-      {language != undefined && Boolean(language) &&
-        <h2>{message[language].commingSoon}</h2>
-      }
+    <div className="container">
+      <div className="aboutme">
+        <h4>{message[language].hi}</h4>
+        <h2>{message.const.name}</h2>
+      <span className="somethinkAboutMe">{message[language].about}</span>
+      </div>
+      <div className="photo">
+      <img className="photo2" src="/myPhoto.jpg" alt="Home" width="40%" />
+      </div>
     </div>
   );
 }
