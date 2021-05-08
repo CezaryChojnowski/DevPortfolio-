b269f8d11e659f7ea6227713f2884fd58b5b30f9
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-05-08 14:06:21 +0200

    Create messages to education component

---
+++ src/messages/educationMessage.js
@@ -0,0 +1,69 @@
+export let message = {
+    secondCycleProgramme: {
+        EN: {
+            graduated:"Computer Science, specializing in Software Engineering",
+            dyscypline:" - Technical Computer Science and Telecommunications",
+            name: "Bialystok Technical University",
+            scienceClub:"Membership in the Math4You science club",
+            schoolarship:"Two-time winner of the rector's scholarship for the best students",
+            dateRange:"03.2020 - present",
+            certificates:"Certificates from science club"
+        },
+        PL: {
+            graduated:"Informatyka, specjalizacja inżynieria oprogramowania",
+            dyscypline:" - Informatyka techniczna i telekomunikacja",
+            name: "Politechnika Białostocka",
+            scienceClub:"Członkostwo w kole naukowym Math4You",
+            schoolarship:"Dwukrotny zdobywca stypendium rektora dla najlepszych studentów",
+            dateRange:"03.2020 - obecnie",
+            certificates:"Zaświadczenia z koła naukowego"
+        }
+    },
+    firstCycleProgramme: {
+        EN: {
+            graduated:"Computer Science and Econometrics",
+            dyscypline:" - Technical Computer Science and Telecommunications",
+            name: "Bialystok Technical University",
+            scienceClub:"Membership in the Math4You science club",
+            bti:"1st place in the Białystok Test of IT specialists in the algorithmic category",
+            speaker:"Speaker at the international conference Mathematics of Borderlands",
+            schoolarship:"Two-time winner of the rector's scholarship for the best students",
+            certificate:"Certificate from science club",
+            btiCertificate:"Białystok Test of IT",
+            speakerCertificate:"Certificate from the Mathematics of Borderlands conference",
+        },
+        PL: {
+            graduated:"Informatyka i ekonometria",
+            dyscypline:" - Informatyka techniczna i telekomunikacja",
+            name: "Politechnika Białostocka",
+            scienceClub:"Członkostwo w kole naukowym Math4You",
+            bti:"1 miejsce w konkursie Białostocki Test Informatyków w kategorii algorytmika",
+            speaker:"Prelegent na międzynarodowej konferencji Mathematics of Borderlands",
+            schoolarship:"Dwukrotny zdobywca stypendium rektora dla najlepszych studentów",
+            certificate:"Zaświadczenie z koła naukowego",
+            btiCertificate:"Białostocki Test Informatyków",
+            speakerCertificate:"Zaświadczenie z konferencji Mathematics of Borderlands",
+        },
+        const:{
+            dateRange:"09.2016 - 02.2020"
+        }
+    }, technicalSchool: {
+        EN: {
+            profile:"IT profile",
+            name: "Complex of Mechanical and General Schools No. 5 in Łomża",
+        },
+        PL: {
+            profile:"Profil IT",
+            name: "Zespół Szkół Mechanicznych i Ogólnokształcących nr 5 w Łomży",
+        },
+        const:{
+            dateRange:"09.2012 - 04.2016"
+        }
+    },
+    PL: {
+        documents: "Dokumenty"
+    },
+    EN: {
+        documents: "Documents"
+    }
+}
